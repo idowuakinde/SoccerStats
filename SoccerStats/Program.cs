6861830446410a6a9264a695ba6c176968d3250c
idowuakinde <8018104+idowuakinde@users.noreply.github.com>
2017-12-10 03:11:25 +0100

    Initial check-in of module SoccerStats

---
+++ SoccerStats/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace SoccerStats
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Idowu Akinde!");
+        }
+    }
+}
