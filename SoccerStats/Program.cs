6020aa52a79823a1c1b327742c76b4055301ce40
idowuakinde <8018104+idowuakinde@users.noreply.github.com>
2017-12-10 07:21:12 +0100

    Added capability for reading a CSV line by line.

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace SoccerStats
@@ -11,21 +12,61 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
 
-            var fileName = Path.Combine(directory.FullName, "data.txt");
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
             var file = new FileInfo(fileName);
 
             if (file.Exists)
             {
-                using (var reader = new StreamReader(file.FullName))
+                var fileRows = ReadSoccerResults(fileName);
+                foreach (var row in fileRows)
                 {
-                    Console.SetIn(reader);
-                    Console.WriteLine(Console.ReadLine());
+                    Console.WriteLine("The name of the Soccer Club is: " + row[1]);
                 }
 
-                Console.ReadLine();
+                //var fileContents = ReadFile(fileName);
+                //string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                //foreach (var line in fileLines)
+                //{
+                //    Console.WriteLine(line);
+                //}
+            }
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static List<string[]> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<string[]>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    soccerResults.Add(values);
+                }
+
+                //while (reader.Peek() > -1)
+                //{
+                //    string soccerResultRow = reader.ReadLine();
+                //    soccerResults.Add(soccerResultRow);
+                //}
+
+                ////var fileRows = ReadFile(fileName).Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                ////foreach (var soccerResultRow in fileRows)
+                ////{
+                ////    soccerResults.Add(soccerResultRow);
+                ////}
             }
 
+            return soccerResults;
         }
     }
 }
-
